396ece8b997ac7979e000b5e50af4dd0243e44dd
Keith Tuttle <klktj01@gmail.com>
2021-12-12 17:43:17 -0500

    more functions and test cases

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,28 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+
+	@Test
+	public void tesSubtract() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subtract(5, 3), 2);
+	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+	}
+
+	@Test
+	public void testDivideByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(5, 0), 0);
+	}
+
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10, 2), 5);
+	}
 }
